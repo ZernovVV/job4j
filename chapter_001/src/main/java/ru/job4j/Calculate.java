c15e992ec1272ff79cc15cf350c68c0ffba02f60
ZernovVV <vorksha@gmail.com>
2018-02-01 13:19:49 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,5 +1,16 @@
 package ru.job4j;
+
+/**
+ * Class Calculate решение задачи части 1 урока 001
+ * @author zernov
+ * @since 31.01.2018
+ * @version 1
+ */
 public class Calculate {
+
+  /**
+   * Вывод строки в консоль
+   */
   public static void main(String[] args) {
     System.out.println("Hello world");
   }
