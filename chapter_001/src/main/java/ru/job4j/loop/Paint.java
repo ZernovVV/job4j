c09590d9fbf46e87f7f8e24dcee047d63fff0731
ZernovVV <vorksha@gmail.com>
2018-04-05 11:56:55 +0300

    4.5. Рефакторинг кода. [#33459]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,5 +1,8 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
+
+
 /**
  * Построение пирамиды в псевдографике
  * @author vzernov
@@ -8,16 +11,56 @@ package ru.job4j.loop;
  */
 public class Paint {
     /**
+     * Строим правую часть пирамиды
+     * @param height высота пирамиды
+     * @return результат
+     */
+    public String rightTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= column
+        );
+    }
+
+    /**
+     * Строим левую часть пирамиды
+     * @param height высота пирамиды
+     * @return результат
+     */
+    public String leftTrl(int height) {
+        return this.loopBy(
+                height,
+                height,
+                (row, column) -> row >= height - column - 1
+        );
+    }
+
+    /**
      * Строим пирамиду
      * @param height высота пирамиды
      * @return результат
      */
     public String pyramid(int height) {
+        return this.loopBy(
+                height,
+                2 * height - 1,
+                (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+        );
+    }
+
+    /**
+     * Универсальный метод для построения пирамид
+     * @param height высота пирамиды
+     * @param weight ширина пирамиды
+     * @param predict условие проставление галки
+     * @return результат
+     */
+    private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
         StringBuilder screen = new StringBuilder();
-        int weight = 2 * height - 1;
         for (int row = 0; row != height; row++) {
             for (int column = 0; column != weight; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
+                if (predict.test(row, column)) {
                     screen.append("^");
                 } else {
                     screen.append(" ");
--- chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
+++ chapter_001/src/test/java/ru/job4j/loop/PaintTest.java
@@ -12,6 +12,46 @@ import static org.junit.Assert.*;
  */
 public class PaintTest {
     /**
+     * Тест построения правой части пирамиды
+     */
+    @Test
+    public void whenPyramid4Right() {
+        Paint paint = new Paint();
+        String rst = paint.rightTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("^   ")
+                                .add("^^  ")
+                                .add("^^^ ")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    /**
+     * Тест построения левой части пирамиды
+     */
+    @Test
+    public void whenPyramid4Left() {
+        Paint paint = new Paint();
+        String rst = paint.leftTrl(4);
+        System.out.println(rst);
+        assertThat(rst,
+                is(
+                        new StringJoiner(System.lineSeparator(), "", System.lineSeparator())
+                                .add("   ^")
+                                .add("  ^^")
+                                .add(" ^^^")
+                                .add("^^^^")
+                                .toString()
+                )
+        );
+    }
+
+    /**
      * Тест пирамиды высотой 2
      */
     @Test
