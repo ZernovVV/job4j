27bcbeaa4f3081f5a067cc5bccac566961a996fb
ZernovVV <vorksha@gmail.com>
2019-03-26 22:20:41 +0300

    Объединить два массива[#124483] в одном цикле

--- chapter_001/src/main/java/ru/job4j/array/Merge.java
+++ chapter_001/src/main/java/ru/job4j/array/Merge.java
@@ -8,34 +8,31 @@ package ru.job4j.array;
  */
 public class Merge {
     /**
-     *
-     * @param left первый отсортированный массив для объединения
+     * @param left  первый отсортированный массив для объединения
      * @param right второй отсортированный массив для объединения
      * @return возвращает отсортированный объединенный массив
      */
     public int[] merge(int[] left, int[] right) {
         int[] result = new int[left.length + right.length];
         int i = 0, j = 0, index = 0;
-        while (i < left.length && j < right.length) {
-            if (left[i] < right[j]) {
+        while (i < left.length || j < right.length) {
+            if (i < left.length && j < right.length) {
+                if (left[i] < right[j]) {
+                    result[index] = left[i];
+                    i++;
+                } else {
+                    result[index] = right[j];
+                    j++;
+                }
+            } else if (i < left.length) {
                 result[index] = left[i];
                 i++;
-            } else {
+            } else if (j < right.length) {
                 result[index] = right[j];
                 j++;
             }
             index++;
         }
-        while (i < left.length) {
-            result[index] = left[i];
-            index++;
-            i++;
-        }
-        while (j < right.length) {
-            result[index] = right[j];
-            index++;
-            j++;
-        }
-        return  result;
+        return result;
     }
 }
--- chapter_001/src/test/java/ru/job4j/array/MergeTest.java
+++ chapter_001/src/test/java/ru/job4j/array/MergeTest.java
@@ -23,4 +23,30 @@ public class MergeTest {
         int[] expect = new int[] {1, 2, 3, 4};
         assertThat(result, is(expect));
     }
+
+    /**
+     * Тест объединения двух отсортированных массивов, первый массив длиннее
+     */
+    @Test
+    public void whenMergeTwoArraysAndSortWhenFirstArrayLonger() {
+        Merge merged = new Merge();
+        int[] a = new int[] {1, 3, 7, 14, 30, 77, 78, 100, 108, 256};
+        int[] b = new int[] {1, 2, 4, 9, 13, 45, 84};
+        int[] result = merged.merge(a, b);
+        int[] expect = new int[] {1, 1, 2, 3, 4, 7, 9, 13, 14, 30, 45, 77, 78, 84, 100, 108, 256};
+        assertThat(result, is(expect));
+    }
+
+    /**
+     * Тест объединения двух отсортированных массивов, второй массив длиннее
+     */
+    @Test
+    public void whenMergeTwoArraysAndSortWhenSecondArrayLonger() {
+        Merge merged = new Merge();
+        int[] a = new  int[] {0, 2, 3, 5, 8, 11, 17};
+        int[] b = new  int[] {1, 2, 4, 7, 8, 9, 10, 17, 23, 27, 28};
+        int[] result = merged.merge(a, b);
+        int[] expect = new int[] {0, 1, 2, 2, 3, 4, 5, 7, 8, 8, 9, 10, 11, 17, 17, 23, 27, 28};
+        assertThat(result, is(expect));
+    }
 }
